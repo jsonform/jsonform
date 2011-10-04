64d3b0e8f35865470700cc26a813f222e6776979
Thomas Bassetto <tbassetto@gmail.com>
2011-10-04 15:28:40 +0200

    Added support for select

--- test/forms/basic.js
+++ test/forms/basic.js
@@ -1,61 +1,64 @@
-
-
 $("#testform").jsonForm({
-	"schema":{
-        "name":"Product",
-        "properties":
-        {
-                "id":
-                {
-                        "type":"number",
-                        "description":"Product identifier",
-                        "required":true
-                },
-                "name":
-                {
-                        "description":"Name of the product",
-                        "type":"string",
-                        "required":true
-                },
-                "price":
-                {
-                        "type":"number",
-                        "minimum":0,
-                        "required":true
-                }/*,
-                "tags":
-                {
-                        "type":"array",
-                        "items":
-                        {
-                                "type":"string"
-                        }
-                }*/
-        }
-	},
-	"elements":[
-		{
-			"key":"id",
-			"onBlur":function() {
-				alert();
-			}
-		},
-		{
-			"type":"fieldset",
-			"items":[
-				"name"
+  "schema":{
+    "name":"Product",
+    "properties":
+    {
+      "id":
+      {
+        "type":"number",
+        "description":"Product identifier",
+        "required":true
+      },
+      "name":
+      {
+        "description":"Name of the product",
+        "type":"string",
+        "required":true
+      },
+      "price":
+      {
+        "type":"number",
+        "minimum":0,
+        "required":true
+      },
+      "gender":{
+         "type":"string",
+         'enum':["male","female"]
+      }
+      /*,
+      "tags":
+      {
+              "type":"array",
+              "items":
+              {
+                      "type":"string"
+              }
+      }*/
+    }
+  },
+  "elements":[
+    {
+      "key":"id",
+      "onBlur":function() {
+        alert();
+      }
+    },
+    {
+      "type":"fieldset",
+      "items":[
+        "name"
+      ]
+    },
+    "price",
+    "gender",
+    {
+      "type":"submit"
+    }
 
-			]
-		},
-		"price",
-		{
-			"type":"submit"
-		}
-
-	],
-	"onSubmit":function(values) {
-		
-	}
-	
+  ],
+  "onSubmit":function(values) {
+    
+  }
+  
 });
 
