3c83c07729796e746688105fdb7f2d8a5dc5266f
Sylvain Zimmer <sylvain@sylvainzimmer.com>
2011-10-04 14:24:26 +0200

    Added files

---
+++ test/forms/basic.js
@@ -0,0 +1,62 @@
+
+
+$("#testform").jsonForm({
+	"schema":{
+        "name":"Product",
+        "properties":
+        {
+                "id":
+                {
+                        "type":"number",
+                        "description":"Product identifier",
+                        "required":true
+                },
+                "name":
+                {
+                        "description":"Name of the product",
+                        "type":"string",
+                        "required":true
+                },
+                "price":
+                {
+                        "type":"number",
+                        "minimum":0,
+                        "required":true
+                },
+                "tags":
+                {
+                        "type":"array",
+                        "items":
+                        {
+                                "type":"string"
+                        }
+                }
+        }
+	},
+	"elements":[
+		{
+			"key":"id",
+			"onBlur":function() {
+				alert();
+			}
+		},
+		{
+			"type":"fieldset",
+			"items":[
+				"name",
+				"price"
+
+			]
+		},
+		"tags",
+		{
+			"type":"submit"
+		}
+
+	],
+	"onSubmit":function(values) {
+		
+	}
+	
+});
+
