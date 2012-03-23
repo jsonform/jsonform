20c8487f05d0cf0e6695568f094c66e215056b90
François Daoust <fd@tidoust.net>
2012-03-23 17:43:35 +0100

    Form values from options.value used for complex arrays as well.
    Values were not properly used to initialize the form whenever the
    array was more complex than a list of string items. Thanks to this
    update, this should work relatively well ("relatively" because
    arrays remain a kind of hack in JSON form for the time being, a
    little bit of reformatting would be needed to do it well).

---
+++ test/forms/array.js
@@ -0,0 +1,76 @@
+$("#testform").jsonForm({
+  "schema": {
+    "datasources": {
+      "type": "object",
+      "properties": {
+        "ds1": {
+          "type": "array",
+          "title": "ds1",
+          "items": {
+            "type": "string"
+          }
+        },
+        "ds2": {
+          "type": "array",
+          "title": "ds2",
+          "items": {
+            "type": "string"
+          }
+        },
+        "ds3": {
+          "type": "array",
+          "title": "ds1",
+          "items": {
+            "type": "string"
+          }
+        }
+      }
+    }
+  },
+  "form": [
+    {
+      "type": "array",
+      "items": [
+        {
+          "type": "fieldset",
+          "legend": "Entry XXXidxXXX",
+          "items": [
+            {
+              "key": "datasources.ds1[]",
+              "title": "First ds XXXidxXXX"
+            },
+            "datasources.ds2[]",
+            "datasources.ds3[]"
+          ]
+        }
+      ]
+    },
+    {
+      "type": "actions",
+      "items": [
+        {
+          "type": "submit",
+          "value": "Submit"
+        }
+      ]
+    }
+  ],
+  "value": {
+    "datasources": {
+      "ds1": [
+        "toto",
+        "tutu"
+      ],
+      "ds2": [
+        null,
+        "truc"
+      ],
+      "ds3": [
+        "blah"
+      ]
+    }
+  },
+  "onSubmit": function (errors,values) {
+    console.log(errors,values);
+  }
+});
