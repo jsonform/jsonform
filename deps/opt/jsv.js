1b6d753b8bb062121332afecd284f2261f8a6ade
François Daoust <francois@joshfire.com>
2013-05-22 11:45:04 +0200

    Added support for "uri" and "email" format validation in JSV
    Schema keys of type "string" may define an additional "format"
    property set to "uri" or "email". These constraints were ignored
    by the JSON Schema validator. They are now enforced using regular
    expressions.
    
    Note the "uri" really means absolute uri. Relative URIs are rejected.
    The scheme may be just about anything.

--- deps/opt/jsv.js
+++ deps/opt/jsv.js
@@ -1,4 +1,6 @@
 // This is a build of https://github.com/garycourt/JSV at 0aa11852537069b0830569ef1eab11a36b65b3ab with jsv.js, schema03 and URI.js appended.
+// The build contains a few lines of custom code to handle format validation.
+// Custom code is wrapped in comments that start with "JOSHFIRE"
 (function(global, require) {
 var exports = {};
 
@@ -2672,7 +2674,24 @@ var exports = exports || this,
 					}
 				},
 
-				"formatValidators" : {}
+				// JOSHFIRE: added a couple of format validators for "uri" and "email"
+				"formatValidators" : {
+					"uri": function (instance, report) {
+						// Regular expression from @diegoperini taken from:
+						// http://mathiasbynens.be/demo/url-regex
+						// ... and only slightly adjusted for use in JavaScript
+						var reUri = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+\-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+\-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/i;
+						return !!reUri.test(instance._value);
+					},
+					"email": function (instance, report) {
+						// Regular expression taken from:
+						// http://www.regular-expressions.info/email.html
+						var reEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+						return !!reEmail.test(instance._value);
+					}
+				}
+				// JOSHFIRE: end of custom code
+				// "formatValidators": {}
 			},
 
 			"contentEncoding" : {
